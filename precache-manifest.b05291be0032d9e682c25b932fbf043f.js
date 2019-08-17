00c0191fcae39fa48eb56c36edd7912e33a0b6fd
AutomatedTester <david.burns@theautomatedtester.co.uk>
2019-08-18 00:22:08 +0100

    Updates

---
+++ precache-manifest.b05291be0032d9e682c25b932fbf043f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64cd966c5c52b16082533c60bee9fa99",
+    "url": "/rubikstraining/index.html"
+  },
+  {
+    "revision": "eccb62225288dce34b8d",
+    "url": "/rubikstraining/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f1e3e3e5430a7938ac0a",
+    "url": "/rubikstraining/static/js/2.2fb2e43c.chunk.js"
+  },
+  {
+    "revision": "eccb62225288dce34b8d",
+    "url": "/rubikstraining/static/js/main.3b2f1d05.chunk.js"
+  },
+  {
+    "revision": "239121de6263abe9d812",
+    "url": "/rubikstraining/static/js/runtime~main.b23cbf7b.js"
+  }
+]);
